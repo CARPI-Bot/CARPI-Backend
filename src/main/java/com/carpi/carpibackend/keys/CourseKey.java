47f3ecacf6b631801ecdc50c8d936d47ce43bd85
Raymond Chen <42894676+SameriteRL@users.noreply.github.com>
2024-11-01 15:21:13 -0400

    Implement equals() and hashCode() for CourseKey

--- src/main/java/com/carpi/carpibackend/keys/CourseKey.java
+++ src/main/java/com/carpi/carpibackend/keys/CourseKey.java
@@ -18,4 +18,23 @@ public class CourseKey implements Serializable {
 
     @Column(name = "code_num", insertable = false, updatable = false, nullable = false)
     private int code;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof CourseKey) {
+            CourseKey other = (CourseKey) obj;
+            return department.equals(other.department) && code == other.code;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = department.hashCode();
+        result = 31 * result + Integer.hashCode(code);
+        return result;
+    }
 }
